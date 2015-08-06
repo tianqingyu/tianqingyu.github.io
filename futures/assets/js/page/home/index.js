5c8e87f354293d206a0c2575b275c5f6c22e7e9d
jun.yj <jun.yj@alibaba-inc.com>
2015-08-06 23:43:51 +0800

    futures

---
+++ futures/assets/js/page/home/index.js
@@ -0,0 +1,47 @@
+/**
+ * @author jun.yj
+ * @since 2015/8/6
+ */
+
+jQuery(function( $ ){
+
+    var $p = $('div.mod-carousel'),
+        $ul = $p.children('ul');
+
+    var $li = $ul.find('li:eq(0)'),
+        $dot = $p.find('span.dot:eq(0)');
+
+    $ul.on('swipeleft', function( evt ){
+
+        var $next = $li.next();
+
+        if ( $next[0] ) {
+
+            $next.css({left: 0});
+
+            $li = $next;
+
+            setCurrDot( $dot, $dot = $dot.next() );
+        }
+
+    }).on('swiperight', function( evt ){
+
+        var $prev = $li.prev();
+
+        if ( $prev[0] ) {
+
+            $li.css({left: '100%'});
+
+            $li = $prev;
+
+            setCurrDot( $dot, $dot = $dot.prev() );
+        }
+    });
+
+    function setCurrDot( $sibling, $curr ) {
+
+        $sibling.removeClass('curr');
+
+        $curr.addClass('curr');
+    }
+});
