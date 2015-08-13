05e67bb012cacbeb3c562b7e0b4dfc5e305ec132
jun.yj <jun.yj@alibaba-inc.com>
2015-08-13 23:40:42 +0800

    futures

---
+++ futures/assets/js/biz/page/trade/done.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author jun.yj
+ * @since 15/8/7
+ */
+
+define(function(require, exports, module){
+    'use strict';
+
+    var loader = require('modLoader');
+
+    loader([
+        'page/trade/trade'
+    ]);
+});
