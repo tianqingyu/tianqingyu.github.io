5f2a9ac5c52b0ed67bf5b251772920a9652c8ac4
jun.yj <jun.yj@alibaba-inc.com>
2015-08-15 23:14:04 +0800

    futures

--- futures/assets/lib/jquery/main.js
+++ futures/assets/lib/jquery/main.js
@@ -7,7 +7,7 @@
 define(function (require, exports, module) {
     'use strict';
 
-    var $ = require('./base/1.11.2/jquery');
+    var $ = require('./base/2.1.4/jquery');
 
     require('./mobile/1.4.5/jquery');
 
