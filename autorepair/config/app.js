6686594782224f81eea7b4ce2fe2fa6cceb23473
jun.yj <jun.yj@alibaba-inc.com>
2015-12-15 20:42:34 +0800

    auto

---
+++ autorepair/config/app.js
@@ -0,0 +1,12 @@
+/**
+ * @author jun.yj
+ * @email god.yangjun@gmail.com
+ * @date 15/12/14
+ */
+
+seajs.config({
+
+    base: 'file:///Users/molayu/Workspace/Web/demo/autorepair/res/js',
+
+    charset: 'UTF-8'
+});
---
+++ autorepair/res/js/page/index/index.js
@@ -0,0 +1,15 @@
+/**
+ * @author jun.yj
+ * @email god.yangjun@gmail.com
+ * @date 15/12/14
+ */
+
+define(function (require, exports, module) {
+    'use strict';
+
+    return {
+        init: function(){
+            console.log('test');
+        }
+    }
+});
