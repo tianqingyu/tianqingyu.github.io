6498cf34f05cf073eb7ace012e9c72d9ba6ad649
jun.yj <jun.yj@alibaba-inc.com>
2015-12-30 11:45:39 +0800

    autorepair

---
+++ autorepair/res/js/page/enterprise/order-detail.js
@@ -0,0 +1,27 @@
+/**
+ * @author jun.yj
+ * @email god.yangjun@gmail.com
+ * @date 15/12/14
+ */
+
+define(function (require, exports, module) {
+    'use strict';
+
+    var $ = require('$'),
+        _ = require('_'),
+        log = require('log'),
+        art = require('art'),
+        net = require('net');
+
+    return {
+        init: function(){
+            this.initTmpl();
+        },
+
+        initTmpl: function(){
+            net.text('detail.art', function( rs ){
+                $('#page-content').replaceWith( art.compile(rs)({}));
+            });
+        }
+    }
+});
