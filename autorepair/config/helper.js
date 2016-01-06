1334a3631d4b52b872d6e598d7fec63d9f0a3cc6
jun.yj <jun.yj@alibaba-inc.com>
2016-01-06 11:22:10 +0800

    autorepair 2

--- autorepair/config/helper.js
+++ autorepair/config/helper.js
@@ -1,11 +1,10 @@
 /**
- * 加载子模板.
- *
  * @author jun.yj
  * @email god.yangjun@gmail.com
  * @date 16/1/5
  */
 
+// 加载子模板
 template.helper('view', function( path, data ){
     return utils.$include( path, data );
 });
