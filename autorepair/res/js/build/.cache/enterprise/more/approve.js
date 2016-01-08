ef78b99dabf3a7fb848424c7c20457a2ef33a9ec
jun.yj <jun.yj@alibaba-inc.com>
2016-01-08 17:37:33 +0800

    autorepair 2

--- autorepair/res/js/build/.cache/enterprise/more/approve.js
+++ autorepair/res/js/build/.cache/enterprise/more/approve.js
@@ -1,9 +1,8 @@
-/*TMODJS:{"version":10,"md5":"8ae8ada9ab9ce194fddbe61f742d81a6"}*/
+/*TMODJS:{"version":11,"md5":"764a66ba35d5ba0cb7ed03871d1363e2"}*/
 template('enterprise/more/approve',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$string=$utils.$string,view=$helpers.view,title=$data.title,$out='';$out+='<!-- 头部 -->\n';
 $out+=$string(view('mods/header', {title:'费用审批'}));
-$out+='\n\n<!-- 内容 -->\n<section class="page-main main-enterprise-more-approve">\n  <div class="content">\n    <!-- 列表 -->\n    <div class="lists">\n      <a href="approve-detail.html">\n        <ul class="list2">\n          <li>\n            <span>客户：</span>\n            <span>刘大山</span>\n          </li>\n          <li>\n            <span>编码：</span>\n            <span>30000786</span>\n          </li>\n          <li>\n            <span>总价：</span>\n            <span>546元</span>\n          </li>\n        </ul>\n      </a>\n      <a href="approve-detail.html">\n        <ul class="list2">\n          <li>\n            <span>客户：</span>\n            <span>刘大山</span>\n          </li>\n          <li>\n            <span>编码：</span>\n            <span>30000786</span>\n          </li>\n          <li>\n            <span>总价：</span>\n            <span>546元</span>\n          </li>\n        </ul>\n      </a>\n    </div>\n  </div>\n</section>\n\n<!-- 底部 -->\n';
-$out+=$string(view('mods/footer'));
+$out+='\n\n<!-- 内容 -->\n<section class="page-main main-enterprise-more-approve">\n  <div class="content">\n    <!-- 列表 -->\n    <div class="lists">\n      <a href="approve-detail.html">\n        <ul class="list2">\n          <li>\n            <span>客户：</span>\n            <span>刘大山</span>\n          </li>\n          <li>\n            <span>编码：</span>\n            <span>30000786</span>\n          </li>\n          <li>\n            <span>总价：</span>\n            <span>546元</span>\n          </li>\n        </ul>\n      </a>\n      <a href="approve-detail.html">\n        <ul class="list2">\n          <li>\n            <span>客户：</span>\n            <span>刘大山</span>\n          </li>\n          <li>\n            <span>编码：</span>\n            <span>30000786</span>\n          </li>\n          <li>\n            <span>总价：</span>\n            <span>546元</span>\n          </li>\n        </ul>\n      </a>\n    </div>\n  </div>\n</section>';
 return new String($out);
 });
--- autorepair/res/js/build/.cache/enterprise/more/orders-search.js
+++ autorepair/res/js/build/.cache/enterprise/more/orders-search.js
@@ -1,9 +1,8 @@
-/*TMODJS:{"version":2,"md5":"d1291358c45c86882d822be4ba3c7539"}*/
+/*TMODJS:{"version":3,"md5":"ac6229fe04eea2c5c7a233c515f71bff"}*/
 template('enterprise/more/orders-search',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$string=$utils.$string,view=$helpers.view,title=$data.title,$out='';$out+='<!-- 头部 -->\n';
 $out+=$string(view('mods/header', {title:'订单查询'}));
-$out+='\n\n<!-- 内容 -->\n<section class="page-main main-enterprise-orders-search">\n  <div class="content">\n    <!-- 表单 -->\n    <ul class="forms-list">\n      <li class="clearfix">\n        <label>姓名</label>\n        <input type="text" placeholder=""/>\n      </li>\n      <li class="clearfix">\n        <label>车牌号</label>\n        <input type="text" placeholder=""/>\n      </li>\n      <li class="clearfix">\n        <label>开始时间</label>\n        <input type="text" placeholder=""/>\n      </li>\n      <li class="clearfix">\n        <label>结束时间</label>\n        <input type="text" placeholder=""/>\n      </li>\n    </ul>\n\n    <!-- action -->\n    <div class="action">\n      <a href="orders.html" class="button">查询</a>\n    </div>\n  </div>\n</section>\n\n<!-- 底部 -->\n';
-$out+=$string(view('mods/footer'));
+$out+='\n\n<!-- 内容 -->\n<section class="page-main main-enterprise-orders-search">\n  <div class="content">\n    <!-- 表单 -->\n    <ul class="forms-list">\n      <li class="clearfix">\n        <label>姓名</label>\n        <input type="text" placeholder=""/>\n      </li>\n      <li class="clearfix">\n        <label>车牌号</label>\n        <input type="text" placeholder=""/>\n      </li>\n      <li class="clearfix">\n        <label>开始时间</label>\n        <input type="text" placeholder=""/>\n      </li>\n      <li class="clearfix">\n        <label>结束时间</label>\n        <input type="text" placeholder=""/>\n      </li>\n    </ul>\n\n    <!-- action -->\n    <div class="action">\n      <a href="orders.html" class="button">查询</a>\n    </div>\n  </div>\n</section>';
 return new String($out);
 });
